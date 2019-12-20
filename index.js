d14d186283321e4a2e9a23ae47d8f9f87f13e631
Ravshan <ravkart@gmail.com>
2019-12-19 16:47:57 -0800

    added shuffle generator

--- index.js
+++ index.js
@@ -1,14 +1,72 @@
 const { Engine, Render, Runner, World, Bodies } = Matter;
 
+const cells = 3;
+const width = 600;
+const height = 600;
+
 const engine = Engine.create();
 const { world } = engine;
 const render = Render.create({
   element: document.body,
   engine: engine,
   options: {
-    width: 800,
-    height: 600
+    //  wireframes: false,
+    width: width,
+    height: height
   }
 });
 Render.run(render);
-Runnder.run(Runner.create(), engine);
+Runner.run(Runner.create(), engine);
+
+//Walls
+const walls = [
+  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
+  Bodies.rectangle(width, height / 2, 40, height, { isStatic: true })
+];
+
+World.add(world, walls);
+
+//Maze generation
+
+const shuffle = arr => {
+  let counter = arr.length;
+
+  while (counter > 0) {
+    const index = Math.floor(Math.random() * counter);
+    counter--;
+
+    const temp = arr[counter];
+    arr[counter] = arr[index];
+    arr[index] = temp;
+  }
+};
+const grid = Array(cells)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const verticals = Array(cells)
+  .fill(null)
+  .map(() => Array(cells - 1).fill(false));
+
+const horizontals = Array(cells - 1)
+  .fill(null)
+  .map(() => Array(cells).fill(false));
+
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
+
+const stepThroughCell = (row, column) => {
+  if (grid[row][column]) {
+    return;
+  }
+  grid[row][column] = true;
+
+  const neighbors = shuffle([
+    [row - 1, column],
+    [row, column + 1],
+    [row + 1, column],
+    [row, column - 1]
+  ]);
+};
